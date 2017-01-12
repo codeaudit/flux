df495bf720e12229ecda295954d33cc98e41e322
Paul Bellamy <paul.a.bellamy@gmail.com>
2017-01-12 16:53:52 +0000

    Implement and test registry.HostRateLimitedRoundTripper

--- registry/rate_limit.go
+++ registry/rate_limit.go
@@ -1,15 +1,23 @@
 package registry
 
 import (
+	"errors"
 	"net/http"
 	"sync"
 	"time"
 )
 
+var (
+	ErrTooManyPendingRequests = errors.New("too many pending requests")
+
+	// For testing
+	now = time.Now
+)
+
 type hostRateLimitedRoundTripper struct {
 	roundTripper http.RoundTripper
+	maxBacklog   time.Duration
 	limits       map[string]limit
-	now          func() time.Time // for testing.
 	sync.RWMutex
 }
 
@@ -18,11 +26,16 @@ type limit struct {
 	nextRequestAt        time.Time
 }
 
-func HostRateLimitedRoundTripper(r http.RoundTripper, limits map[string]int) http.RoundTripper {
+// HostRateLimitedRoundTripper is a http.RoundTripper which applies throttling
+// to requests on a per-host basis.
+// * r          -- upstream roundtripper
+// * maxBacklog -- 1 return ErrTooManyPendingRequests if a request would be kept waiting longer than this. (<= 0 is no limit)
+// * limits     -- the maximum request/second for each host. If <= 0 or unset, no limit for this host.
+func HostRateLimitedRoundTripper(r http.RoundTripper, maxBacklog time.Duration, limits map[string]int) http.RoundTripper {
 	rlc := &hostRateLimitedRoundTripper{
 		roundTripper: r,
+		maxBacklog:   maxBacklog,
 		limits:       map[string]limit{},
-		now:          time.Now,
 	}
 	for reg, max := range limits {
 		rlc.limits[reg] = limit{maxRequestsPerSecond: max}
@@ -39,12 +52,17 @@ func (c *hostRateLimitedRoundTripper) RoundTrip(r *http.Request) (*http.Response
 	c.RUnlock()
 	if limit.maxRequestsPerSecond > 0 {
 		c.Lock()
-		now := c.now()
-		if limit.nextRequestAt.Before(now) {
-			limit.nextRequestAt = now
+		t := now()
+		if limit.nextRequestAt.Before(t) {
+			limit.nextRequestAt = t
 		}
-		sleep = limit.nextRequestAt.Sub(now)
-		limit.nextRequestAt = limit.nextRequestAt.Add(1 * time.Second / time.Duration(limit.maxRequestsPerSecond))
+		sleep = limit.nextRequestAt.Sub(t)
+		newNextRequest := limit.nextRequestAt.Add(1 * time.Second / time.Duration(limit.maxRequestsPerSecond))
+		if c.maxBacklog > time.Duration(0) && newNextRequest.After(t.Add(c.maxBacklog)) {
+			c.Unlock()
+			return nil, ErrTooManyPendingRequests
+		}
+		limit.nextRequestAt = newNextRequest
 		c.limits[host] = limit
 		c.Unlock()
 	}
--- registry/rate_limit_test.go
+++ registry/rate_limit_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestRateLimitedRoundTripper_OnlyAllowsMaxRequestsPerSecondToARegistry(t *testing.T) {
+	t.Parallel()
 	// It should only allow max requests/second to a registry
 	requests := []time.Time{}
 	var rt http.RoundTripper = roundtripperFunc(func(r *http.Request) (*http.Response, error) {
@@ -16,23 +17,17 @@ func TestRateLimitedRoundTripper_OnlyAllowsMaxRequestsPerSecondToARegistry(t *te
 	})
 	host := "example.local"
 	limit := 3
-	rt = HostRateLimitedRoundTripper(rt, map[string]int{host: limit})
-	var wg sync.WaitGroup
-	wg.Add(limit + 2)
+	rt = HostRateLimitedRoundTripper(rt, 0, map[string]int{host: limit})
 	for i := 0; i < limit+2; i++ {
-		go func() {
-			request, err := http.NewRequest("GET", "http://"+host+"/image/foo", nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			_, err = rt.RoundTrip(request)
-			if err != nil {
-				t.Fatal(err)
-			}
-			wg.Done()
-		}()
+		request, err := http.NewRequest("GET", "http://"+host+"/image/foo", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = rt.RoundTrip(request)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
-	wg.Wait()
 
 	buckets := map[int64]int{}
 	for _, ts := range requests {
@@ -43,22 +38,86 @@ func TestRateLimitedRoundTripper_OnlyAllowsMaxRequestsPerSecondToARegistry(t *te
 	}
 }
 
-func TestRateLimitedRoundTripper_DifferentReposShareALimite(t *testing.T) {
-	// Different repos (on the same registry) should share a limit
-	t.Error("TODO")
-}
-
-func TestRateLimitedRoundTripper_DifferentRegistriesEnforcedSeparately(t *testing.T) {
+func TestRateLimitedRoundTripper_DifferentHostsEnforcedSeparately(t *testing.T) {
+	t.Parallel()
 	// Separate registries should have be enforced separately
-	t.Error("TODO")
-}
+	var lock sync.Mutex
+	requests := map[string][]time.Time{}
+	var rt http.RoundTripper = roundtripperFunc(func(r *http.Request) (*http.Response, error) {
+		lock.Lock()
+		defer lock.Unlock()
+		requests[r.URL.Host] = append(requests[r.URL.Host], time.Now())
+		return nil, nil
+	})
+	limits := map[string]int{
+		"host1": 1,
+		"host2": 2,
+		"host3": 3,
+	}
+	rt = HostRateLimitedRoundTripper(rt, 0, limits)
 
-func TestRateLimitedRoundTripper_UnlimitedRegistries(t *testing.T) {
-	// Unlimited registries should be unlimited
-	t.Error("TODO")
+	var wg sync.WaitGroup
+	wg.Add(len(limits))
+	for host, limit := range limits {
+		go func(h string, lim int) {
+			for i := 0; i < lim+1; i++ {
+				request, err := http.NewRequest("GET", "http://"+h+"/image/foo", nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				_, err = rt.RoundTrip(request)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+			wg.Done()
+		}(host, limit)
+	}
+	wg.Wait()
+
+	for host, limit := range limits {
+		buckets := map[int64]int{}
+		for _, ts := range requests[host] {
+			buckets[ts.Unix()]++
+			if buckets[ts.Unix()] > limit {
+				t.Error("Too many requests/second to " + host)
+			}
+		}
+	}
 }
 
 func TestRateLimitedRoundTripper_BacklogTooHigh(t *testing.T) {
+	t.Parallel()
 	// If the backlog is too high, an error should be returned.
-	t.Error("TODO")
+	var rt http.RoundTripper = roundtripperFunc(func(r *http.Request) (*http.Response, error) {
+		return nil, nil
+	})
+	host := "example.local"
+	limit := 1
+	maxBacklog := 2 * time.Second
+	rt = HostRateLimitedRoundTripper(rt, maxBacklog, map[string]int{host: limit})
+
+	// Lock now, so it will be like all the requests arrive at the same time.
+	currentTime := time.Now()
+	now = func() time.Time { return currentTime }
+
+	var errs []error
+	for i := 0; i < int(maxBacklog/time.Second)+1; i++ {
+		request, err := http.NewRequest("GET", "http://"+host+"/image/foo", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if _, err := rt.RoundTrip(request); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) < 1 {
+		t.Errorf("Expected >=1 error, got %d", len(errs))
+	}
+	for _, err := range errs {
+		if err != ErrTooManyPendingRequests {
+			t.Errorf("Expected ErrTooManyPendingRequests error, got %q", err)
+		}
+	}
 }
